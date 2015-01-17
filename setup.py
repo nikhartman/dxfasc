eb580fb748c6ef6d928c3c40dde054014e1f21fc
nikhartman <nik.hartman@gmail.com>
2015-01-17 17:05:31 -0500

    add .gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = 'dxfasc',
+      version = '1.0',
+      description = 'dxf to asc conversion tools for ebeam lithography',
+      author = 'Nik Hartman',
+      author_email = 'nik.hartman@gmail.com',
+      url = 'https://github.com/nikhartman/dxfasc',
+      py_modules = ['dxfasc']
+      )
